81d61fba1c170c8eeb04399959b88049c513a994
PavelSkorikov <spg75@yandex.ru>
2019-06-27 21:16:04 +0300

    first

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-tabs
+      v-model="tab"
+      inline-label
+      class="bg-primary text-white shadow-2"
+    >
+      <q-tab name="auth" icon="people" label="Авторизация" />
+    </q-tabs>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
